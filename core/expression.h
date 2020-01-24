cec4db59887755e2c72671e92f67103c3b6bfffd
lfenart <lucas.fenart@protonmail.com>
2020-01-24 14:25:05 +0100

    Initial commit

---
+++ core/expression.h
@@ -0,0 +1,14 @@
+//
+// Created by lucasf on 24/01/2020.
+//
+
+#ifndef FUZZY_EXPRESSION_H
+#define FUZZY_EXPRESSION_H
+
+template<class T>
+class Expression {
+public:
+    T evaluate() = 0;
+};
+
+#endif //FUZZY_EXPRESSION_H
