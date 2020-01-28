17a5ee8a7fa1dbade47285b07e887078e43181d1
Bahrallil <khadija.bahrallil@uha.fr>
2020-01-28 13:34:05 +0100

    isTriangle

--- fuzzy/isTriangle.h
+++ fuzzy/isTriangle.h
@@ -2,7 +2,95 @@
 // Created by mac air on 28/01/2020.
 //
 
+#include "operators.h"
+
+
 #ifndef FUZZY_ISTRIANGLE_H
 #define FUZZY_ISTRIANGLE_H
+namespace fuzzy {
+
+    template<typename T>
+    class IsTriangle : public Is<T> {
+
+    private:
+        T min;
+        T mid;
+        T max;
+
+    public:
+        explicit IsTriangle(const T &_min, const T &_mid, const T &_max);
+
+        T evaluate(core::Expression<T> *expression) const ;
+
+        const T &getMin() const;
+
+        const T &getMid() const;
+
+        const T &getMax() const;
+
+        void setMin(const T &_min);
+
+        void setMid(const T &_mid);
+
+        void setMax(const T &_max);
+    };
+
+    template<typename T>
+    IsTriangle<T>::IsTriangle(const T &_min, const T &_mid, const T &_max) : min(_min), mid(_mid), max(_max) {
+    }
+
+    template<typename T>
+    T IsTriangle<T>::evaluate(core::Expression<T> *expression) const {
+
+
+
+        T value = expression->evaluate();
+
+        if (value <= min) {
+
+            return 0;
+
+        } else if (value >= min && value <= mid) {
+
+            return (value - min) / (mid - min);
+
+        } else if (value >= mid && value <= max) {
+
+            return (max - value) / (max - mid);
+        }
+
+        return 0;
+    }
+
+    template<typename T>
+    const T &IsTriangle<T>::getMin() const {
+        return min;
+    }
+
+    template<typename T>
+    const T &IsTriangle<T>::getMid() const {
+        return mid;
+    }
+
+    template<typename T>
+    const T &IsTriangle<T>::getMax() const {
+        return max;
+    }
+
+    template<typename T>
+    void IsTriangle<T>::setMin(const T &_min) {
+        min = _min;
+    }
+
+    template<typename T>
+    void IsTriangle<T>::setMid(const T &_mid) {
+        mid = _mid;
+    }
+
+    template<typename T>
+    void IsTriangle<T>::setMax(const T &_max) {
+        max = _max;
+    }
+}
 
 #endif //FUZZY_ISTRIANGLE_H
