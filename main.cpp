74837e12f932900c76a1014e8b1a34475ffa7350
lfenart <lucas.fenart@protonmail.com>
2020-01-28 12:13:55 +0100

    clean main

--- main.cpp
+++ main.cpp
@@ -2,19 +2,32 @@
 
 #include "core/ValueModel.h"
 #include "core/BinaryExpression.h"
-#include "core/UnaryExpression.h"
 #include "fuzzy/OrMax.h"
 #include "fuzzy/OrPlus.h"
+#include "fuzzy/AggMax.h"
+#include "fuzzy/AggPlus.h"
+
+typedef double num_t;
 
 int main() {
-    core::ValueModel<int> v;
-    v.setValue(1);
-    core::ValueModel<int> v1;
-    v1.setValue(5);
-    fuzzy::OrMax<int> orMax;
-    fuzzy::OrPlus<int> orPlus;
-    std::cout << v.evaluate() << std::endl;
-    std::cout << orMax.evaluate(&v,&v1) << std::endl;
-    std::cout << orPlus.evaluate(&v,&v1) << std::endl;
+    core::ValueModel<num_t> v1(1);
+    std::cout << v1.evaluate() << std::endl;
+
+    core::ValueModel<num_t> v2;
+    v2.setValue(5);
+    std::cout << v2.evaluate() << std::endl;
+
+    fuzzy::OrMax<num_t> orMax;
+    std::cout << orMax.evaluate(&v1, &v2) << std::endl;
+
+    fuzzy::OrPlus<num_t> orPlus;
+    std::cout << orPlus.evaluate(&v1, &v2) << std::endl;
+
+    fuzzy::AggMax<num_t> aggMax;
+    std::cout << aggMax.evaluate(&v1, &v2) << std::endl;
+
+    fuzzy::AggPlus<num_t> aggPlus;
+    std::cout << aggPlus.evaluate(&v1, &v2) << std::endl;
+
     return 0;
 }
