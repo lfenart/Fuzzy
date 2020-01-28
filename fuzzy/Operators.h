316e69c4d66b3b7cd61cb063adb91ae11ffcf6de
Bahrallil <khadija.bahrallil@uha.fr>
2020-01-28 14:52:06 +0100

    fix case problem

--- fuzzy/Operators.h
+++ fuzzy/Operators.h
@@ -1,31 +1,26 @@
 //
-// Created by Lucas Schloesslin on 28/01/2020.
+// Created by mac air on 28/01/2020.
 //
 
 #ifndef FUZZY_OPERATORS_H
 #define FUZZY_OPERATORS_H
 
+
 #include "../core/Expression.h"
 #include "../core/BinaryExpression.h"
+#include "../core/UnaryExpression.h"
 
 namespace fuzzy {
 
-    template<class T>
-    class Or : public core::BinaryExpression<T> {
-    };
 
-    template<class T>
-    class Agg : public core::BinaryExpression<T> {
-    };
-    
-    
     template<typename T>
     class And : public core::BinaryExpression<T> {
 
     public:
         virtual T evaluate(core::Expression<T> *left, core::Expression<T> *right) const = 0;
-    };
 
+
+    };
     template<typename T>
     class Is : public core::UnaryExpression<T> {
 
@@ -33,6 +28,8 @@ namespace fuzzy {
         virtual T evaluate(core::Expression<T> *expression) const = 0;
     };
 
+
 }
+
 
 #endif //FUZZY_OPERATORS_H
