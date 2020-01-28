741d2cb6a09952fd023bdcd4ef384905e540e1fa
Schloesslin <schloesslin.lucas@gmail.com>
2020-01-28 11:05:24 +0100

    Unary Binary

---
+++ core/BinaryExpression.h
@@ -0,0 +1,20 @@
+//
+// Created by Lucas Schloesslin on 28/01/2020.
+//
+
+#ifndef FUZZY_BINARYEXPRESSION_H
+#define FUZZY_BINARYEXPRESSION_H
+
+#include "Expression.h"
+
+namespace core {
+
+    template<class T>
+    class BinaryExpression {
+    public:
+        virtual T evaluate(Expression<T>*, Expression<T>*) const = 0;
+    };
+
+}
+
+#endif //FUZZY_BINARYEXPRESSION_H
---
+++ fuzzy/Operator.h
@@ -0,0 +1,12 @@
+//
+// Created by Lucas Schloesslin on 28/01/2020.
+//
+
+#ifndef FUZZY_OPERATOR_H
+#define FUZZY_OPERATOR_H
+
+namespace fuzzy{
+
+}
+
+#endif //FUZZY_OPERATOR_H
