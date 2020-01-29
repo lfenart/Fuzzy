85f017bc7aadc0e54fb29744b3ce12214f1611a6
Bahrallil <khadija.bahrallil@uha.fr>
2020-01-29 10:55:09 +0100

    BinaryShadowExpression

---
+++ core/BinaryShadowExpression.h
@@ -0,0 +1,59 @@
+//
+// Created by mac air on 29/01/2020.
+//
+
+#ifndef FUZZY_BINARYSHADOWEXPRESSION_H
+#define FUZZY_BINARYSHADOWEXPRESSION_H
+
+#include <iostream>
+#include "BinaryExpression.h"
+namespace core {
+    template<class T>
+    class BinaryShadowExpression : public BinaryExpression<T> {
+
+    private:
+        BinaryExpression <T> *target;
+
+    public:
+        explicit BinaryShadowExpression(BinaryExpression <T> *_target);
+
+        T evaluate(Expression <T> *left, Expression <T> *right) const ;
+
+        const BinaryExpression <T> *getTarget() const;
+
+        BinaryExpression <T> *getTarget();
+
+        void setTarget(BinaryExpression <T> *_target);
+    };
+
+    template<class T>
+    BinaryShadowExpression<T>::BinaryShadowExpression(BinaryExpression <T> *_target) : target(_target) {
+    }
+
+    template<class T>
+    T BinaryShadowExpression<T>::evaluate(Expression <T> *left, Expression <T> *right) const {
+
+        if (target == nullptr) {
+            std::cout << "nullptr" << std::endl;
+        }
+
+        return target->evaluate(left, right);
+    }
+
+    template<class T>
+    const BinaryExpression <T> *BinaryShadowExpression<T>::getTarget() const {
+        return target;
+    }
+
+    template<class T>
+    void BinaryShadowExpression<T>::setTarget(BinaryExpression <T> *_target) {
+        target = _target;
+    }
+
+    template<class T>
+    BinaryExpression <T> *BinaryShadowExpression<T>::getTarget() {
+
+        return target;
+    }
+}
+#endif //FUZZY_BINARYSHADOWEXPRESSION_H
