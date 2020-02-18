de0fd334d887ae174a9dd85ecec2688c3f37e238
lfenart <lucas.fenart@protonmail.com>
2020-02-18 09:16:13 +0100

    NaryExpressionModel

---
+++ core/NaryExpressionModel.h
@@ -0,0 +1,58 @@
+//
+// Created by lucasf on 18/02/2020.
+//
+
+#ifndef FUZZY_NARYEXPRESSIONMODEL_H
+#define FUZZY_NARYEXPRESSIONMODEL_H
+
+#include <vector>
+#include "NaryExpression.h"
+
+namespace core {
+
+    template<class T>
+    class NaryExpressionModel {
+    public:
+        NaryExpressionModel(std::vector<NaryExpression<T> *>);
+
+        virtual ~NaryExpressionModel();
+
+        T evaluate() const;
+
+        T evaluate(const std::vector<Expression<T> *> &) const;
+
+    private:
+        NaryExpression<T> *op;
+        std::vector<Expression<T> *> operands;
+    };
+
+    template<class T>
+    NaryExpressionModel<T>::NaryExpressionModel(std::vector<NaryExpression<T> *> _operands): operands(_operands) {}
+
+    template<class T>
+    NaryExpressionModel<T>::~NaryExpressionModel() {
+        // ?
+        for (typename std::vector<Expression<T> *>::iterator it = operands.begin(); it != operands.end(); it++) {
+            delete *it;
+        }
+    }
+
+    template<class T>
+    T NaryExpressionModel<T>::evaluate() const {
+        if (op != nullptr) {
+            return evaluate(operands);
+        }
+        throw "";
+    }
+
+    template<class T>
+    T NaryExpressionModel<T>::evaluate(const std::vector<Expression<T> *> &operands) const {
+        if (operands.size() != 0) {
+            return op->evaluate(operands);
+        }
+        throw "";
+    }
+
+}
+
+#endif //FUZZY_NARYEXPRESSIONMODEL_H
